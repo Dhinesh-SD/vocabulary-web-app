b1f32c4e977b1ff7b30b1db1d99787bd6f29ca60
LykeWise <54202433+LykeWise@users.noreply.github.com>
2019-10-16 17:10:26 +0530

    Updates

---
+++ precache-manifest.35d33cb84292c3b8cb5dda1df5a5b41d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb534f2f6c8f2d6df50d8b8787e22634",
+    "url": "/vocabulary-web-app/index.html"
+  },
+  {
+    "revision": "58a786f758642a254460",
+    "url": "/vocabulary-web-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "71a30d6f8cb49972848f",
+    "url": "/vocabulary-web-app/static/js/2.bbd6406e.chunk.js"
+  },
+  {
+    "revision": "58a786f758642a254460",
+    "url": "/vocabulary-web-app/static/js/main.de52d9a5.chunk.js"
+  },
+  {
+    "revision": "4c0c7f9ae35c606e6382",
+    "url": "/vocabulary-web-app/static/js/runtime-main.0ccd147e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/vocabulary-web-app/static/media/logo.25bf045c.svg"
+  }
+]);
